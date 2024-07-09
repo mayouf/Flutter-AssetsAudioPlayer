8dc27fecfc7ccf0d993fe6449673187aad1de3ce
mayouf <mayouf.k@gmail.com>
2024-07-09 11:51:29 +0200

    Update NotificationManager.kt
    fix bug `ForegroundServiceStartNotAllowedException ` from this stackTrace:
    
    ```
    Fatal Exception: android.app.ForegroundServiceStartNotAllowedException: Service.startForeground() not allowed due to mAllowStartForeground false: service com.palace.knossos/com.github.florent37.assets_audio_player.notification.NotificationService
           at android.app.ForegroundServiceStartNotAllowedException$1.createFromParcel(ForegroundServiceStartNotAllowedException.java:54)
           at android.app.ForegroundServiceStartNotAllowedException$1.createFromParcel(ForegroundServiceStartNotAllowedException.java:50)
           at android.os.Parcel.readParcelableInternal(Parcel.java:4789)
           at android.os.Parcel.readParcelable(Parcel.java:4757)
           at android.os.Parcel.createExceptionOrNull(Parcel.java:3020)
           at android.os.Parcel.createException(Parcel.java:3009)
           at android.os.Parcel.readException(Parcel.java:2992)
           at android.os.Parcel.readException(Parcel.java:2934)
           at android.app.IActivityManager$Stub$Proxy.setServiceForeground(IActivityManager.java:6995)
           at android.app.Service.startForeground(Service.java:797)
           at com.github.florent37.assets_audio_player.notification.NotificationService.displayNotification(NotificationService.kt:350)
           at com.github.florent37.assets_audio_player.notification.NotificationService.access$displayNotification(NotificationService.kt)
           at com.github.florent37.assets_audio_player.notification.NotificationService$displayNotification$1.invokeSuspend(NotificationService.kt:147)
           at kotlin.coroutines.jvm.internal.BaseContinuationImpl.resumeWith(ContinuationImpl.kt:33)
           at kotlinx.coroutines.DispatchedTask.run(DispatchedTask.kt:104)
           at android.os.Handler.handleCallback(Handler.java:942)
           at android.os.Handler.dispatchMessage(Handler.java:99)
           at android.os.Looper.loopOnce(Looper.java:223)
           at android.os.Looper.loop(Looper.java:324)
           at android.app.ActivityThread.main(ActivityThread.java:8486)
           at java.lang.reflect.Method.invoke(Method.java)
           at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:582)
           at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1061)
    
    ```

--- android/src/main/kotlin/com/github/florent37/assets_audio_player/notification/NotificationManager.kt
+++ android/src/main/kotlin/com/github/florent37/assets_audio_player/notification/NotificationManager.kt
@@ -1,54 +1,72 @@
 package com.github.florent37.assets_audio_player.notification
-
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.content.ContextCompat
 import com.github.florent37.assets_audio_player.AssetsAudioPlayerPlugin
+import java.lang.ref.WeakReference
 
-class NotificationManager(private val context: Context) {
-
-    var closed = false
+class NotificationManager(context: Context) {
+    private val contextRef = WeakReference(context)
+    @Volatile
+    private var closed = false
 
     fun showNotification(playerId: String, audioMetas: AudioMetas, isPlaying: Boolean, notificationSettings: NotificationSettings, stop: Boolean, durationMs: Long) {
+        if (closed) return
+
+        val context = contextRef.get() ?: return
+
         try {
-            if (closed)
-                return
             if (stop) {
                 stopNotification()
             } else {
-                context.startService(Intent(context, NotificationService::class.java).apply {
+                val intent = Intent(context, NotificationService::class.java).apply {
                     putExtra(NotificationService.EXTRA_NOTIFICATION_ACTION, NotificationAction.Show(
-                            isPlaying = isPlaying,
-                            audioMetas = audioMetas,
-                            playerId = playerId,
-                            notificationSettings = notificationSettings,
-                            durationMs = durationMs
+                        isPlaying = isPlaying,
+                        audioMetas = audioMetas,
+                        playerId = playerId,
+                        notificationSettings = notificationSettings,
+                        durationMs = durationMs
                     ))
-                })
+                }
+                startForegroundService(context, intent)
             }
             AssetsAudioPlayerPlugin.instance?.assetsAudioPlayer?.registerLastPlayerWithNotif(playerId)
         } catch (t: Throwable) {
-            t.printStackTrace()
+            Log.e("NotificationManager", "Error showing notification", t)
         }
     }
 
     fun stopNotification() {
+        val context = contextRef.get() ?: return
+
         try {
-            context.startService(Intent(context, NotificationService::class.java).apply {
-                putExtra(NotificationService.EXTRA_NOTIFICATION_ACTION, NotificationAction.Hide(
-                ))
-            })
+            val intent = Intent(context, NotificationService::class.java).apply {
+                putExtra(NotificationService.EXTRA_NOTIFICATION_ACTION, NotificationAction.Hide())
+            }
+            startForegroundService(context, intent)
         } catch (t: Throwable) {
-            t.printStackTrace()
+            Log.e("NotificationManager", "Error stopping notification", t)
         }
     }
 
     fun hideNotificationService(definitively: Boolean = false) {
+        val context = contextRef.get() ?: return
+
         try {
-            //if remainingNotif == 0, stop
             context.stopService(Intent(context, NotificationService::class.java))
             closed = definitively
         } catch (t: Throwable) {
-            t.printStackTrace()
+            Log.e("NotificationManager", "Error hiding notification service", t)
+        }
+    }
+
+    private fun startForegroundService(context: Context, intent: Intent) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ContextCompat.startForegroundService(context, intent)
+        } else {
+            context.startService(intent)
         }
     }
 }
